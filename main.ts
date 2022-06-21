9803868e9f46fd87bf2ec322c562ccc22ce5cc5b
Billy Kirk <kirk.william.c@gmail.com>
2022-06-21 16:35:22 -0500

    Removing languages from my version

--- main.ts
+++ main.ts
@@ -1,13 +1,6 @@
 import blog from "https://raw.githubusercontent.com/billykirk01/deno_blog/main/blog.tsx";
 // import blog from "/home/billy/Development/deno_blog/blog.tsx";
 
-
-import "https://esm.sh/prismjs@1.27.0/components/prism-typescript?no-check";
-import "https://esm.sh/prismjs@1.27.0/components/prism-go?no-check";
-import "https://esm.sh/prismjs@1.27.0/components/prism-rust?no-check";
-import "https://esm.sh/prismjs@1.27.0/components/prism-bash?no-check";
-import "https://esm.sh/prismjs@1.27.0/components/prism-python?no-check";
-
 blog({
   title: "Billy Kirk",
   description: "I love building applications for the web primarily with Deno 🦕 and Svelte 🧡 and I occasionally dabble in systems programming with Rust 🦀 and Go 💙",
